bc2d64a1e30fc3e2a92a257b906f7b24e81cb52c
nowosad20p <91263440+nowosad20p@users.noreply.github.com>
2022-05-26 09:29:35 +0200

    tak

--- getPlayerNameHint.php
+++ getPlayerNameHint.php
@@ -5,11 +5,13 @@ require("functions.php");
 $allPlayers = scandir("users");
 if (strlen($_GET["cur"]) > 0) {
     foreach ($allPlayers as $value) {
+            if($value!=".htaccess"){
         if (strpos($value, $_GET["cur"]) !== false && $value != $_SESSION["user"]) {
             if (!doesFileInclude("users/" . $_SESSION["user"] . "/friendList.txt", $_GET["cur"])) {
                 echo "<li><input type=button value='" . $value . "' class='addFriendBtn'></li>";
             }
         }
+            }
     }
 } else {
     echo "brak wyników";
